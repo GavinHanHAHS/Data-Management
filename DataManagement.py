50a83a4316c65c8eb4645f19fdf4c741ebc4027a
GavinHanHAHS <55220804+GavinHanHAHS@users.noreply.github.com>
2022-06-03 11:30:25 -0600

    figure out how tf dictionaries work

--- DataManagement.py
+++ DataManagement.py
@@ -5,32 +5,29 @@ import json
 
 # Preload Data
 # Cookie Data
-filecook = open("cookie.txt.", "r")
-cookie_data = filecook.read().split("\n")  # process file into a list format
+filecook = open("cookies.txt.", "r")
+cookie_data = json.load(filecook)
 print(cookie_data[0:5])  # check data has loaded properly
 
 
 
-# Helper Functions
-def display_data():
-    #    for item in cookie_data:
-    #        print()
-    print("\n".join(cookie_data))
-
 
 # Main Loop for Program run
-#program_run = True
+program_run = True
 
-#while program_run:
-#    x = int(input("What Would you like to do?\n"
-#                  "1. Display all data\n"
-#                  "2. Display some Data\n"
-#                  " 2.1 Select Filter\n"
-#                  "3. Add Current/Remove Obj to Favourites\n"
-#                  "4. Display Favourites\n"))
-#
-#    if x == 1:
-#        display_data()
+while program_run:
+    x = int(input("What Would you like to do?\n"
+                  "1. Display all data\n"
+                  "2. Display some Data\n"
+                  " 2.1 Select Filter\n"
+                  "3. Add Current/Remove Obj to Favourites\n"
+                  "4. Display Favourites\n"))
+
+    if x == 1:
+        for cookie in cookie_data:
+            for key in cookie:
+                print(f"{key}: {cookie[key]}")
+            print("\n")
 
 # Ideas for this short program:
 # Filters
@@ -41,32 +38,38 @@ def display_data():
 #   add desc for each cookie, display when choosing
 #   how to index items to look at? have each display also be a list with accessible objects
 
-all_cookies = []
 
 
-def new_cookie(CookieList):
+
+
+# Below is mostly just the code for transforming cookie.txt into cookies.txt
+
+#all_cookies = []
+
+
+#def new_cookie(CookieList):
     # Input a list that is one line of txt doc, output cookie dictionary.
     # Format for cookies is Name, Rarity, Price and Description
-    newDict = {
-        "Name": CookieList[0] + " Cookie",
-        "Rarity": CookieList[1],
-        "Price": CookieList[2],
-        "Description": CookieList[3]
-    }
+#    newDict = {
+#        "Name": CookieList[0] + " Cookie",
+#        "Rarity": CookieList[1],
+#        "Price": CookieList[2],
+#        "Description": CookieList[3]
+#    }
 
-    all_cookies.append(newDict)
+#    all_cookies.append(newDict)
 
 
-for cookie in cookie_data:
-    x = cookie.split(",")
-    new_cookie(x)
+#for cookie in cookie_data:
+#    x = cookie.split(",")
+#    new_cookie(x)
 
 
 #for cookie in all_cookies:
 #    print(str(cookie) + "\n")
 
-x = json.dumps(all_cookies, indent=4)
-print(x)
-y = open("cookies.txt", "w")
-y.write(x)
-y.close
+#x = json.dumps(all_cookies, indent=4)
+#print(x)
+#y = open("cookies.txt", "w")
+#y.write(x)
+#y.close
